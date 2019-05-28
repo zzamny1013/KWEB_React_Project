a332628afdabfa8cce37fb70c0662bf64ec0ff0f
zzamny1013 <msyh0207@naver.com>
2019-05-28 13:44:44 +0900

    May 28

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { useReducer } from 'react';
 import './App.css';
 import gook1 from './gook1.jpg';
+import logo from './logo.JPG';
+import produce from 'immer';
 
 function reducer(state, action){
   switch(action.type){
     case 'INTRODUCE':
       console.log(state.introducev);
-      return { ...state, [state.introducev]: !state.introducev};
+      return produce(state, draft=>{
+        draft.introducev = true;
+        draft.menuv = false;
+        draft.bookv = false;
+        draft.promotionv = false;
+      });
     case 'MENU':
-      return { ...state, [state.menuv]: !state.menuv};
+      console.log(state.menuv);
+      return produce(state, draft=>{
+        
+        draft.introducev = false;
+        draft.menuv = true;
+        draft.bookv = false;
+        draft.promotionv = false;
+      });
     case 'BOOK':
-      return { ...state, [state.bookv]: !state.bookv};
+      console.log(state.bookv);
+      return produce(state, draft=>{
+        
+        draft.introducev = false;
+        draft.menuv = false;
+        draft.bookv = true;
+        draft.promotionv = false;
+      });
     case 'PROMOTION':
-      return { ...state, [state.promotionv]: !state.promotionv};
+        console.log(state.promotionv);
+      return produce(state, draft=>{
+        draft.introducev = false;
+        draft.menuv = false;
+        draft.bookv = false;
+        draft.promotionv = true;
+      });
     default:
       return state;
   }
@@ -29,14 +56,17 @@ function App(){
 
   return(
     <div>
-      <img className='logo' src={gook1} alt='logo'/>
+      <img className='logo' src={logo} alt='logo'/>
       <div className='menubar'>
         <button className='menubarButton' onClick={()=>{dispatch({ type: 'INTRODUCE'})}}>소개</button>
-        <button className='menubarButton'>메뉴</button>
-        <button className='menubarButton'>예약</button>
-        <button className='menubarButton'>프로모션</button>
+        <button className='menubarButton' onClick={()=>{dispatch({ type: 'MENU'})}}>메뉴</button>
+        <button className='menubarButton' onClick={()=>{dispatch({ type: 'BOOK'})}}>예약</button>
+        <button className='menubarButton' onClick={()=>{dispatch({ type: 'PROMOTION'})}}>프로모션</button>
       </div>
       {state.introducev && <img src='./intro.jpg' alt='intro'/>}
+      {state.menuv && <img src='./menu.jpg' alt='menu'/>}
+      {state.bookv && <img src='./book.jpg' alt='book'/>}
+      {state.promotionv && <img src='./promotion.jpg' alt='promotion'/>}
 
     </div>
   );
